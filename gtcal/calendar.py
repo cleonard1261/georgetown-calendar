83474295291baba97b457428edb0d2f234d0c0e5
Chad Leonard <chadleonard@CWL2.local>
2014-04-03 22:43:32 -0400

    added update_event to calender.py

--- gtcal/calendar.py
+++ gtcal/calendar.py
@@ -25,6 +25,12 @@ import csv
 import os
 
 ##########################################################################
+## Global Variables
+##########################################################################
+
+
+
+##########################################################################
 ## Main Calendar App
 ##########################################################################
 
@@ -47,9 +53,9 @@ class Calendar(object):
 		 return csvfile
 
     def remove_event(self, eventdate, eventname, calname):
-        gtdir = '/Users/chadleonard/venv/calendar_app/georgetown_calendar/gtcal/'
-        file_name = gtdir + calname + '.csv'
-        output_file = gtdir + calname + 'Temp.csv'
+        file_dir = '/Users/chadleonard/venv/calendar_app/georgetown_calendar/gtcal/'
+        file_name = file_dir+calname+'.csv'
+        output_file = file_dir+calname+'Temp.csv'
         input_file = open(file_name, 'r')
         with open(output_file, 'wb') as csvfile:
 			self.csvwriter = csv.writer(csvfile, dialect='excel', delimiter='|')
@@ -73,11 +79,41 @@ class Calendar(object):
         # http://stackoverflow.com/questions/7588934/deleting-columns-in-a-csv-with-python
         # see jointest.py for os.system commads to rm the old file
 
-    def update_event(self):
-        pass
+    def update_event(self,eventdate, eventname, eventtime, calname):
+        file_dir = '/Users/chadleonard/venv/calendar_app/georgetown_calendar/gtcal/'
+        file_name = file_dir+calname+'.csv'
+        output_file = file_dir+calname+'Temp.csv'        
+        input_file = open(file_name, 'r')
+        with open(output_file, 'wb') as csvfile:
+			self.csvwriter = csv.writer(csvfile, dialect='excel', delimiter='|')
+			reader = csv.reader(input_file, delimiter='|', quoting=csv.QUOTE_NONE)
+			for row in reader:
+				if eventdate in [row][0]:
+				   if eventtime in [row][0]:
+				      self.csvwriter.writerow([eventdate]+[eventname]+[eventtime])
+				   else:
+				       self.csvwriter.writerow([row][0])
+				else:
+				    self.csvwriter.writerow([row][0])
+        input_file.close()
+        csvfile.close()	
+        cmd = 'mv ' + output_file+' '+file_name		
+        os.system(cmd)
 
-    def todays_agenda(self):
-        pass
+
+    def todays_agenda(self, eventdate, calname):
+        file_dir = '/Users/chadleonard/venv/calendar_app/georgetown_calendar/gtcal/'
+        file_name = file_dir+calname+'.csv'    
+        input_file = open(file_name, 'r')
+        reader = csv.reader(input_file, delimiter='|', quoting=csv.QUOTE_NONE)
+        agenda = []
+        for row in reader:
+			if eventdate in [row][0]:
+				agenda.append([row][0])
+			else:
+				continue
+        input_file.close()
+        return agenda
 
     def order_agenda(self):
         pass
@@ -90,7 +126,7 @@ if __name__ == '__main__':
 
     event = Event("Cal Test Event", '2013-03-30', "13:30")
     event1 = Event("Cal Test Event One", '2014-04-02', "11:30")
-    event2 = Event("Cal Test Event Two", '2014-04-03', "14:30")
+    event2 = Event("Cal Test Event Two", '2014-04-02', "14:30")
     print event1.eventname
     csvfile = cal.add_event(event, cal.calname)
     cal.save(csvfile)
@@ -99,6 +135,10 @@ if __name__ == '__main__':
     csvfile = cal.add_event(event2, cal.calname)
     cal.save(csvfile)
     cal.remove_event(event1.eventdate, event1.eventname, cal.calname)
+    cal.update_event(event2.eventdate, "Cal Prod Event Two", event2.eventtime, cal.calname)
+    agenda = cal.todays_agenda(event2.eventdate, cal.calname)
+    for i in agenda:
+        print i[0]+',', i[1]+',',i[2]
 
 
 
